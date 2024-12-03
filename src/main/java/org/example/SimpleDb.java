316f09e64821d368709cd38a24649ad05bee837b
sangxxjin <sj_0368@naver.com>
2024-12-03 16:37:26 +0900

    blue 1

--- src/main/java/org/example/SimpleDb.java
+++ src/main/java/org/example/SimpleDb.java
@@ -1,17 +1,13 @@
 package org.example;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class SimpleDb {
 
     private final String host;
     private final String username;
     private final String password;
     private final String dbName;
-
-    public SimpleDb(String host, String username, String password, String dbName) {
-        this.host = host;
-        this.username = username;
-        this.password = password;
-        this.dbName = dbName;
-    }
 
 }
